6692e800819932f97edb5751439462954bd32f82
aprxty3 <ajiprxty3@gmail.com>
2023-03-23 02:37:46 +0700

    implement await

--- bin/async_await.dart
+++ bin/async_await.dart
@@ -10,12 +10,24 @@ Future<String> last() async {
   return 'Prasetyo';
 }
 
+Future<void> say() async {
+  var firstName = await first();
+  var lastName = await last();
+  var fullname = await sayHello('Dwi $firstName $lastName ');
+  print(fullname);
+}
+
 void main() {
+  /// TODO: sulit dibaca
   first()
       .then((value1) => last().then((value2) {
             var fullname = '$value1 $value2';
             return sayHello(fullname);
           }))
       .then((response) => print(response));
+
+//TODO : lebih mudah dibaca
+  say();
+
   print('helloooo');
 }
