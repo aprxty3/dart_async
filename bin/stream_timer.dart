0c3fc86671d807793a0aa95c046dc5847b74a735
aprxty3 <ajiprxty3@gmail.com>
2023-03-23 02:16:00 +0700

    timer periodic

--- bin/stream_timer.dart
+++ bin/stream_timer.dart
@@ -4,5 +4,12 @@ void main() {
   Timer(Duration(seconds: 2), () {
     print('hello');
   });
+
+  Timer.periodic(Duration(seconds: 1), (timer) {
+    print('timer: ${timer.tick}');
+    if (timer.tick == 5) {
+      timer.cancel();
+    }
+  });
   print('done');
 }
