34fcee09d603a8a14c37716e17ca770178b5e4a1
aprxty3 <ajiprxty3@gmail.com>
2023-03-23 02:07:48 +0700

    broadcast stream

--- bin/broadcast_stream.dart
+++ bin/broadcast_stream.dart
@@ -1,1 +1,16 @@
-void main() {}
+Stream<int> numbers() {
+  return Stream.periodic(Duration(seconds: 2), (i) => i);
+}
+
+void main() {
+  numbers().asBroadcastStream().take(5);
+
+  numbers().listen((event) {
+    print(event);
+  });
+  numbers().listen((event) {
+    print(event);
+  });
+
+  print('done');
+}
