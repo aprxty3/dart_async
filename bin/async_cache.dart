3393c46fbf02ac3a90d8b7f451082cd93fc3b54d
aprxty3 <ajiprxty3@gmail.com>
2023-03-23 21:17:00 +0700

    async cache

--- bin/async_cache.dart
+++ bin/async_cache.dart
@@ -3,15 +3,18 @@ import 'package:async/async.dart';
 Future<String> getData() {
   return Future.delayed(
     Duration(seconds: 2),
-    () => 'Dapet dataa',
+    () {
+      print('calculating data');
+      return 'Dapet dataa';
+    },
   );
 }
 
 void main() async {
-  final asyncCache = AsyncCache<String>(Duration(seconds: 10));
+  final asyncCache = AsyncCache<String>(Duration(seconds: 20));
 
-  final result1 = await asyncCache.fetch(() => getData());
+  String result1 = await asyncCache.fetch(() => getData());
   print(result1);
-  final result2 = await asyncCache.fetch(() => getData());
+  String result2 = await asyncCache.fetch(() => getData());
   print(result2);
 }
