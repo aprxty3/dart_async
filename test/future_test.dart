6534a4681eedb8fddcb2e560dbf59f19e7056ec8
aprxty3 <ajiprxty3@gmail.com>
2023-03-23 21:51:34 +0700

    future matcher

--- test/future_test.dart
+++ test/future_test.dart
@@ -4,7 +4,18 @@ Future<String> getName() async {
   return 'ajii prasetyo';
 }
 
+Future<String> matcher() async {
+  return Future.delayed(
+    Duration(seconds: 2),
+    () => 'ajiii prasetyo',
+  );
+}
+
 void main() {
+  test('Future Matcher', () {
+    final name = matcher();
+    expect(name, completion(equals('ajiii prasetyo')));
+  });
   test('Future Test', () async {
     final name = await getName();
     expect(name, 'ajii prasetyo');
